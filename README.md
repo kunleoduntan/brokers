e304332f25c955e07d83ecaeff6fc1df8a719ef5
erpnext <erpnext@erpnext-127215-erp-0>
2025-08-07 05:15:53 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## brokers
+
+brokers
+
+#### License
+
+mit
---
+++ brokers/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "brokers",
+			"type": "module",
+			"label": _("brokers")
+		}
+	]
